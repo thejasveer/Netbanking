9c251b89659d6b5be4304ed91fca19986337fee3
thejasveer13 <thejasveer13@gmail.com>
2024-06-15 10:52:16 -0500

    pr

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -32,12 +32,15 @@ export function App() {
     }),
   );
 
+
+
+
   return (
     <BrowserRouter >
         <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         {/* Your app here */}
-       
+       <Appbar/>
         <Routes>
           <Route path="/banks/:token" element={<Banks/>} /> {/* 👈 Renders at /app/ */}
         </Routes>
@@ -48,8 +51,31 @@ export function App() {
     </BrowserRouter>
   );
 }
+
+
+function  Appbar(){
+
+  return <div className='flex gap-2 text-xl justify-center items-center shadow-md shadow-blue-400 p-4'>
+    <div className='flex items-center'><span>W</span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+</svg>
+<span>llet</span></div>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+</svg>
+
+    <div className='flex items-center rounded-md p-1 gap-1'><svg xmlns="http://www.w3.org/2000/svg" fill="bg-blue-100 " viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-blue-300 p-0.5 animate-pulse  rounded-full size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+</svg><span>Netbanking</span>
+
+</div>
+
+  </div>
+}
+
+
 function Center({children}:{children:any}){
-  return <div className='flex justify-center items-center  min-h-screen overflow-auto '>
+  return <div className='flex justify-center items-center  min-h-screen max-w-96 mx-auto overflow-auto '>
     {children}
   </div>
 }
@@ -116,9 +142,9 @@ function BankLogo({bank,onClick ,signin}:{bank: bankType[0],onClick:any,signin?:
 function BankLogin({bank,onClick}:{bank:bankType[0]|null;onClick:any}){
   console.log(bank?.placeholder)
 
-  return <div className='flex flex-col gap-2'>
+  return <div className='flex flex-col gap-5 -top-5'>
 
-    <div className='flex gap-1 items-center mb-10 max-w-max absolute top-2 left-4' onClick={()=>onClick(null)}>
+    <div className='flex gap-1 items-center   max-w-max  left-4' onClick={()=>onClick(null)}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
       </svg>
